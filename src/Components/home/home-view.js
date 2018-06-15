9ad87787760804470517229eb063aeaacabc8835
agrawalaayushi <aayushi.agrawal13@gmail.com>
2018-06-15 06:49:14 +0530

    Home component created

---
+++ src/Components/home/home-view.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        This is home page
+      </div>
+    );
+  }
+}
+
+export default HomePage; 
