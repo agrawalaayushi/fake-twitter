832758befe96cf43222c95055e0b67703b103d09
agrawalaayushi <aayushi.agrawal13@gmail.com>
2018-06-15 06:47:36 +0530

    code clean

--- src/Components/header/header-view.js
+++ src/Components/header/header-view.js
@@ -8,7 +8,7 @@ class HeaderView extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Fake Twitter</h1>
+          <div className="App-title">Welcome to Fake Twitter</div>
         </header>
       </div>
     );
