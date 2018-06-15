01d36a0447f1a0a0d2709683d9413f1b5ba404cf
agrawalaayushi <aayushi.agrawal13@gmail.com>
2018-06-15 05:44:39 +0530

    header view added in login component

--- src/Components/header/header-view.js
+++ src/Components/header/header-view.js
@@ -6,7 +6,10 @@ class HeaderView extends Component {
   render() {
     return (
       <div className="App">
-       
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Fake Twitter</h1>
+        </header>
       </div>
     );
   }
