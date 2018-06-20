b1ef933bb80b3f85907b7c58f4f28c1804f50098
agrawalaayushi <aayushi.agrawal13@gmail.com>
2018-06-20 10:43:42 +0530

    retweet functionality added

--- src/Components/modal/retweet-modal-view.js
+++ src/Components/modal/retweet-modal-view.js
@@ -1,54 +1,82 @@
 import React, { Component } from 'react';
 import '../../App.css';
-import { Button, Icon, Modal, Input} from 'semantic-ui-react'
+import { Button, Modal, Input} from 'semantic-ui-react'
 
 class RetweetModalView extends Component {
   constructor(props){
     super(props); 
     this.state={
 			open: false,
-      isError: false,
-      tweet: "",
-		}
-		this.close = this.close.bind(this);
+      selectedTweet: "",
+      retweetComment: "",
+    }
+    this.close = this.close.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
 	//-----------------------------------
   // Methods
   //-----------------------------------
 
+  // To close the modal
   close(){
     this.setState({
       open: false,
-      isError: false
+      retweetComment: ""
 		});
 	}
 
+  // To open the modal
 	showRetweetModal(tweet){
-    // recieve previous tweet here
 		this.setState({ 
       open: true,
-      tweet: tweet,
-		});
+      selectedTweet: tweet,     // recieve selected tweet here
+    });
   }
   
+  handleInputChange(e) {
+    this.setState({retweetComment: e.target.value});
+  }
+
+  // On click retweet button, send the required fields to Parent class 
+  handleRetweet(e){
+    e.preventDefault();    
+    const { retweetComment, selectedTweet } = this.state;
+      let params =[
+        {
+          id: 5,
+          name : selectedTweet.name,
+          description : retweetComment,
+          selectedTweet : selectedTweet.description
+        }
+      ] 
+      this.props.submitRetweetCallback(params);
+  }
+
   //-----------------------------------
   // Views
   //-----------------------------------
+  
 	addCommentView(){
     return(
-      <Input placeholder="Add a comment"/>
+      <div className="retweet-comment-box">
+        <Input placeholder="Add a comment.." value={this.state.retweetComment} 
+        onChange={this.handleInputChange}/>
+      </div>
     )
 	}
 
 	tweetDescription(data){
     return(
-      <p>{data.description}</p>
+      <div className="retweet-description-box" >
+        <h5 className="user-name">@{data.name}</h5>
+        <p>{data.description}</p>
+      </div>
     )
 	}
 
 	getModalView(){
-    const { open, tweet } = this.state;
+    const { open, selectedTweet } = this.state;
     return(
       <Modal className='retweet-modal' size='tiny' open={open} closeIcon onClose={this.close}>
         <Modal.Header>
@@ -57,11 +85,11 @@ class RetweetModalView extends Component {
         <Modal.Content>
           <div className="modal-content-header">
 						{this.addCommentView()}
-            {this.tweetDescription(tweet)}
+            {this.tweetDescription(selectedTweet)}
           </div>
         </Modal.Content>
 				<Modal.Actions>
-					<Button color='blue'>Retweet</Button>
+					<Button color='blue' onClick= {(e) => this.handleRetweet(e)}>Retweet</Button>
 				</Modal.Actions>
       </Modal>
     )
