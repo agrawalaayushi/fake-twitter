7cf20b36af52091dff46e7e2c90b0c3f1b13ccb5
agrawalaayushi <aayushi.agrawal13@gmail.com>
2018-06-20 10:43:02 +0530

    Routers added in App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
-import LoginView from './auth-sign-in/login-view';
+import {
+	HashRouter,
+	Route
+} from 'react-router-dom';
+
+import LoginPage from './auth-sign-in/login-view';
+import HomePage from './home/home-view';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-intro">
-          <LoginView />
+          <HashRouter>
+            <div> 
+              <Route exact path="/" render={(props)=><LoginPage {...props}/>}/>
+              <Route exact path="/login" render={(props)=><LoginPage {...props}/>} />
+              <Route exact path="/home" render={(props)=><HomePage {...props} />} />
+            </div>
+        	</HashRouter >,
         </div>
       </div>
     );
